97f64aff0511b9e56f0055ffe8a4fad1d040bfd2
Sebastian Gruchacz <sebastian@gruchacz.com>
2018-09-26 22:00:59 +0200

    Podstawowe przykłady

---
+++ src/TrainingShop.Model/Pies.cs
@@ -0,0 +1,26 @@
+﻿namespace TrainingShop.Model
+{
+    using System.Drawing;
+
+    public class Pies
+    {
+        public string Nazwa;
+
+        public Color Kolor;
+
+        public decimal Waga;
+
+        private Żołądek ZawartośćŻołądka;
+
+        public void Jedz(Żarcie jedzenie)
+        {
+            this.ZawartośćŻołądka.Add(jedzenie);
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"Dog \"{this.Nazwa}\" is {this.Kolor} and has weigh {this.Waga}kg";
+        }
+    }
+}
---
+++ src/TrainingShop.Model/Żarcie.cs
@@ -0,0 +1,12 @@
+﻿namespace TrainingShop.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Żarcie
+    {
+    }
+}
